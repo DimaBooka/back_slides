868aee079af76870c7653c3283cb5feb43c6f811
bogdan <spawnvpn@gmail.com>
2016-10-11 11:17:08 +0300

    Created models.

--- slides/models.py
+++ slides/models.py
@@ -6,4 +6,41 @@ class Presentation(models.Model):
     description = models.TextField(max_length=4096)
     slides = models.FileField(upload_to='static/media/presentations')
     thumbnail = models.ImageField()
-    creator = models.ForeignKey()
+    # creator = models.ForeignKey()
+    publish_date = models.DateTimeField()
+    published = models.BooleanField()
+    comments = models.ForeignKey(Commentary)
+
+    def __str__(self):
+        return self.name
+
+
+class Commentary(models.Model):
+    # author = models.ForeignKey()
+    text = models.TextField(max_length=255)
+    publish_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.text
+
+
+class Event(models.Model):
+    PLANNED = 1
+    DONE = 2
+    CANCELED = 3
+    POSTPONED = 4
+
+    STATE_CHOICES = (
+        (PLANNED, "Planned"),
+        (DONE, "Done"),
+        (CANCELED, "Canceled"),
+        (POSTPONED, "Postponed"),
+    )
+
+    name = models.CharField(max_length=40)
+    presentation = models.ForeignKey(Presentation)
+    date = models.DateTimeField()
+    state = models.IntegerField(choices=STATE_CHOICES, default=PLANNED)
+
+    def __str__(self):
+        return self.name
