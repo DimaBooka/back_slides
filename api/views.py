8fa2d56329e040d2b55a2b562027183b0c472c89
bogdan <spawnvpn@gmail.com>
2016-10-13 14:35:55 +0300

    Fixed bug #42.

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
@@ -9,12 +10,11 @@ from rest_framework.decorators import api_view
 from rest_framework.filters import DjangoFilterBackend, OrderingFilter
 from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from api.filters import PresentationFilter, EventFilter, CommentaryFilter
-from api.permissions import IsOwnerOrReadOnly
+from api.permissions import IsOwnerOrStaffOrReadOnly
 from api.serializers import (
     CommentarySerializer,
     EventSerializer,
@@ -43,15 +43,21 @@ def api_root(request, format=None):
 class PresentationViewSet(viewsets.ModelViewSet):
     queryset = Presentation.objects.all()
     serializer_class = PresentationSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAdminUser, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrStaffOrReadOnly, )
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filter_class = PresentationFilter
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return self.queryset
+        else:
+            return self.queryset.filter(Q(published=True) | Q(creator=self.request.user),)
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAdminUser, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrStaffOrReadOnly,)
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filter_class = EventFilter
 
@@ -59,7 +65,7 @@ class EventViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Commentary.objects.all()
     serializer_class = CommentarySerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAdminUser, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrStaffOrReadOnly,)
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filter_class = CommentaryFilter
 
