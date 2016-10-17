2a9841a4408016125c25fc821ba015dd00c9f229
bogdan <spawnvpn@gmail.com>
2016-10-17 12:05:09 +0300

    Fixed bug #53.

--- slides/models.py
+++ slides/models.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db import models
-from django.contrib.sites.models import Site
+
 
 class Presentation(models.Model):
     name = models.CharField(max_length=256)
