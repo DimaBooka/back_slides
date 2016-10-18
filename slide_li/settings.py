9ec9d3887568072e77fcc226211994859372c810
VladBezpalko <vl.bezpalko@gmail.com>
2016-10-18 11:48:15 +0300

    improved append

--- slide_li/settings.py
+++ slide_li/settings.py
@@ -162,9 +162,8 @@ if DEBUG:
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOW_CREDENTIALS = True
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
+    REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] += (
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     )
 
 try:
