462a1b04e13d738fe11e1af8547b4d80222e6068
VladBezpalko <vl.bezpalko@gmail.com>
2016-10-24 16:41:05 +0300

    add fields author and secret in Event model

--- slides/models.py
+++ slides/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 
 class Presentation(models.Model):
@@ -31,18 +32,22 @@ class Event(models.Model):
     DONE = 2
     CANCELED = 3
     POSTPONED = 4
+    LIVE = 5
 
     STATE_CHOICES = (
         (PLANNED, "Planned"),
         (DONE, "Done"),
         (CANCELED, "Canceled"),
         (POSTPONED, "Postponed"),
+        (LIVE, "Live"),
     )
 
     name = models.CharField(max_length=40)
     presentation = models.ForeignKey(Presentation)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     date = models.DateTimeField()
     state = models.IntegerField(choices=STATE_CHOICES, default=PLANNED)
+    secret = models.UUIDField(default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.name
