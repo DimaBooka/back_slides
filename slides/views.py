4ce2040168bec55ebcf156b2f07a0fb563e872ee
VladBezpalko <vl.bezpalko@gmail.com>
2016-11-01 13:36:25 +0200

    ssl fix

--- slides/views.py
+++ slides/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 from slides.models import Event
-
+from django.conf import settings
 from django.views.decorators.clickjacking import xframe_options_exempt
 from slide_li.settings import SOCKET_ADDR
 
@@ -19,6 +19,7 @@ class LivePresentationView(View):
                 'secret': event.secret,
                 'slides': event.presentation.slides,
                 'socket_addr': SOCKET_ADDR,
+                'ssl': settings.SSL,
                 }
         else:
             template = 'wait.html'
