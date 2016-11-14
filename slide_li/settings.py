26c0f3b8ee5670b0ff9ec66142d259921078de9a
DimaBooka <atlasb777@gmail.com.com>
2016-11-14 13:12:48 +0200

    Fix ice server list

--- slide_li/settings.py
+++ slide_li/settings.py
@@ -192,9 +192,41 @@ ACCOUNT_ADAPTER = 'api.adapter.SlidesAccountAdapter'
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
 
+WEBRTC_ICE_SERVERS = [
+    {'url': 'stun:stun01.sipphone.com'},
+    {'url': 'stun:stun.ekiga.net'},
+    {'url': 'stun:stun.fwdnet.net'},
+    {'url': 'stun:stun.ideasip.com'},
+    {'url': 'stun:stun.iptel.org'},
+    {'url': 'stun:stun.rixtelecom.se'},
+    {'url': 'stun:stun.schlund.de'},
+    {'url': 'stun:stun.l.google.com:19302'},
+    {'url': 'stun:stun1.l.google.com:19302'},
+    {'url': 'stun:stun2.l.google.com:19302'},
+    {'url': 'stun:stun3.l.google.com:19302'},
+    {'url': 'stun:stun4.l.google.com:19302'},
+    {'url': 'stun:stunserver.org'},
+    {'url': 'stun:stun.softjoys.com'},
+    {'url': 'stun:stun.voiparound.com'},
+    {'url': 'stun:stun.voipbuster.com'},
+    {'url': 'stun:stun.voipstunt.com'},
+    {'url': 'stun:stun.voxgratia.org'},
+    {'url': 'stun:stun.xten.com'},
+    {
+	    'url': 'turn:192.158.29.39:3478?transport=udp',
+	    'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	    'username': '28224511:1379330808',
+    },
+    {
+	    'url': 'turn:192.158.29.39:3478?transport=tcp',
+	    'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	    'username': '28224511:1379330808',
+    }
+]
+
+REDIS_CON = 'redis://localhost:6379/0'
+
 try:
     from .settings_local import *
 except ImportError:
     pass
-
-REDIS_CON = 'redis://localhost:6379/0'
--- slides/views.py
+++ slides/views.py
@@ -1,9 +1,10 @@
+import json
+
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
 from slides.models import Event
 from django.conf import settings
 from django.views.decorators.clickjacking import xframe_options_exempt
-from slide_li.settings import SOCKET_ADDR
 
 
 class LivePresentationView(View):
@@ -13,8 +14,9 @@ class LivePresentationView(View):
         event = get_object_or_404(Event, id=pk)
         ctx = {
             'id': pk,
-            'socket_addr': SOCKET_ADDR,
+            'socket_addr': settings.SOCKET_ADDR,
             'prefix': 'wss' if settings.SSL else 'ws',
+            'ice_servers': json.dumps(settings.WEBRTC_ICE_SERVERS),
         }
         print(request.user, event.author)
         if event.date_started and not event.date_finished:
